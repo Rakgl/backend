48e70db8d46f10c0da51e389a05115d2757d0c18
Kosalrak <linnorak.k@aditi.asia>
2025-05-17 21:45:02 +0700

    frontend

--- obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6f310829e0c2a1ca9991343fb76bb7dcba67a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
